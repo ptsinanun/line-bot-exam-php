4c7c407db5160d0fbe8bcc2c4d59e760cc1f7e38
ptsinanun <ptsinanun@yahoo.com>
2018-11-15 17:23:36 +0700

    Update sentmassage.php

--- sentmassage.php
+++ sentmassage.php
@@ -6,7 +6,7 @@
    $arrayHeader[] = "Content-Type: application/json";
    $arrayHeader[] = "Authorization: Bearer {$accessToken}";
    //รับข้อความจากผู้ใช้
-   $message = $arrayJson['events'][0]['message']['text'];
+   //$message = $arrayJson['events'][0]['message']['text'];
    //รับ id ของผู้ใช้
    //$id = $arrayJson['events'][0]['source']['userId'];
    $id = "U8e35be7d054e420706555612de0321a0";
