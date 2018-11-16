33f87ae79e2d37588fc8435f1a8452c665778f6f
ptsinanun <ptsinanun@yahoo.com>
2018-11-16 10:19:09 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -14,7 +14,7 @@
     //รับ id ของผู้ใช้
     
     //$request = "format=csv&by=member&rs=hour&rk=productivity&rb=".$month_first_date."&re=".$cur_date = date('Y-m-d');
-    $urlWithoutProtocol = "http://fishlanding.fisheries.go.th/LbVmsErr/api/post/readTotalCheck.php?shipcode = '".$message."'"; 
+    $urlWithoutProtocol = "http://fishlanding.fisheries.go.th/LbVmsErr/api/post/readTotalCheck.php?shipcode = '".$message[0]."'"; 
     $isRequestHeader = FALSE;
 
     $ch = curl_init();
