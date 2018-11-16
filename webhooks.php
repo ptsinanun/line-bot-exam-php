5d74fe470dfcf30e145ce41798d265c96375d266
ptsinanun <ptsinanun@yahoo.com>
2018-11-16 09:40:45 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -24,7 +24,7 @@
     if($message == "สวัสดี"){
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "text";
-        $arrayPostData['messages'][0]['text'] = $followboat[0];//"สวัสดีจ้าาา";
+        $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา".$followboat[0];//"สวัสดีจ้าาา";
         replyMsg($arrayHeader,$arrayPostData);
     }
     #ตัวอย่าง Message Type "Sticker"
