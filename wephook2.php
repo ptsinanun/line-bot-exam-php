b8ce3341d73cb0030c0f6960dda500a56fcaf5cc
ptsinanun <ptsinanun@yahoo.com>
2019-03-15 09:12:18 +0700

    Update wephook2.php

--- wephook2.php
+++ wephook2.php
@@ -79,15 +79,15 @@
         $arrayPostData['messages'][1]['stickerId'] = "131";
         replyMsg($arrayHeader,$arrayPostData);
     }
-#$arrayPostData json_encode($arrayHeader)
-function replyMsg($arrayHeader,$arrayHeader){
+# 
+function replyMsg($arrayHeader,$arrayPostData){
         $strUrl = "https://api.line.me/v2/bot/message/reply";
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL,$strUrl);
         curl_setopt($ch, CURLOPT_HEADER, false);
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);    
-        curl_setopt($ch, CURLOPT_POSTFIELDS,$arrayHeader);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayHeader));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         $result = curl_exec($ch);
