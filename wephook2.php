7748be25544c9984e9a19c063b2da6d41425bd10
ptsinanun <ptsinanun@yahoo.com>
2019-03-15 09:13:38 +0700

    Update wephook2.php

--- wephook2.php
+++ wephook2.php
@@ -87,7 +87,7 @@ function replyMsg($arrayHeader,$arrayPostData){
         curl_setopt($ch, CURLOPT_HEADER, false);
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);    
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayHeader));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayPostData));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         $result = curl_exec($ch);
